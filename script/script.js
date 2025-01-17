d0d8ecabef080ad2d255b149948dc275364e4490
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-17 03:21:39 -0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+//dom selection
+
+const name = `Engel Noel B.N.`;
+const id =`001`;
+
+//display in the dom
+
+const datos = document.querySelector("#messageBox");
+messageBox.innerHTML = `
+My name is : ${name} 
+ID number : ${id} `;
+console.log(`hello world`);
